12902671f39417d2301a5fb6c7fbb496a07e78d6
Rida Mumtaz <mumtazrida6@gmail.com>
2021-09-10 23:46:37 +0530

    Working Project1

---
+++ index.js
@@ -0,0 +1,40 @@
+var buttons = document.querySelectorAll("button");
+var length = buttons.length;
+for (var i=0; i < length; i++){
+document.querySelectorAll("button")[i].addEventListener("click", function(){
+  var pressed = this.innerHTML;
+  makeSound(pressed);
+  buttonAnimation(pressed);
+});
+}
+document.addEventListener("keypress", function(event){
+makeSound(event.key);
+buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch(key){
+    case "w" : var audio = new Audio('sounds/tom-1.mp3');
+             audio.play();
+             break;
+    case "a" : var audio = new Audio('sounds/tom-2.mp3');
+             audio.play();
+             break;
+    case "s" : var audio = new Audio('sounds/tom-3.mp3');
+             audio.play();
+             break;
+    case "d" : var audio = new Audio('sounds/tom-4.mp3');
+             audio.play();
+             break;
+    case "j" : var audio = new Audio('sounds/kick-bass.mp3');
+             audio.play();
+             break;
+    case "k" : var audio = new Audio('sounds/crash.mp3');
+             audio.play();
+             break;
+    case "l" : var audio = new Audio('sounds/snare.mp3');
+             audio.play();
+             break;
+    default: break;
+  }
+}
